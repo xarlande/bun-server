7a48fd9032b0aa3649d035528282fde767222056
bogdan <0966312126a@gmail.com>
2023-09-30 20:30:54 +0300

    update server

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-import {Server} from "bun";
-
 Bun.serve({
     port: 2000,
-    fetch(request: Request, server: Server): Response | Promise<Response> {
+    fetch(request: Request): Response | Promise<Response> {
         const url = new URL(request.url)
         if (url.pathname === '/image')  return new Response(Bun.file('./file/images.jpeg'));
         if (url.pathname === '/html')  return new Response(Bun.file('./file/index.html'));
-        return new Response('Hello I`m Bun server\ni have url:\n /image\n /html');
+        if (url.pathname === '/') return new Response('Hello I`m Bun server\ni have url:\n /image\n /html');
+        return new Response('There is no such request');
     }
 })
